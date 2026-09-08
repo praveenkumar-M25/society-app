79d09a2f1096547b7a98578952bb42fe84a43477
Your Name <you@example.com>
2026-09-08 14:25:57 +0530

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
-}
-
+  apiKey: "AIzaSyCXIf23kwiqmBmnn0fA7a1C4FQzaL4CJ6Q",
+  authDomain: "society-management-173e9.firebaseapp.com",
+  projectId: "society-management-173e9",
+  storageBucket: "society-management-173e9.firebasestorage.app",
+  messagingSenderId: "1062309327615",
+  appId: "1:1062309327615:web:a58dd16bd420739bb052ca",
+  measurementId: "G-T04GBWEN5E"
+};
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
